d61a1622d546dd78afbb9e2e68576b1c86c83458
epcruz <eduardaffpaulo@gmail.com>
2022-04-03 14:16:17 +0200

    set mongoose models

--- index.js
+++ index.js
@@ -1,95 +1,28 @@
+const mongoose = require('mongoose'); //require the Mogoose Package
+const Models = require('./models.js'); // require the file with models I created
+
+const Movies = Models.Movie; //specify which models to use within the models.js file
+const Users = Models.User;
+
+mongoose.connect('mongodb://localhost:27017/myFlixDB',{ //allows Mongoose to connect with MongoDB so it can perform CRUD
+    useNewUrlParser: true, useUnifiedTopology: true
+});
+
 const express = require('express');
+const bodyParser = require('body-parser');
     morgan = require('morgan');
-    bodyParser = require('body-parser'),
     uuid = require('uuid');
 
 const app = express();
 
-let users = [
-    {
-        id: 1,
-        name: 'Kim',
-        favouriteMovies: [],
-    },
-    {
-        id: 2,
-        name: 'Joe',
-        favouriteMovies: [],
-    }
-]
-
-let movies = [
-    {
-        title: 'Inglourious Basterds',
-        description: 'In Nazi-occupied France during World War II, a plan to assassinate Nazi leaders by a group of Jewish U.S. soldiers coincides with a theatre owner\'s vengeful plans for the same.',
-        genre: {
-            name: 'Drama',
-            description: 'In film and television, drama is a category of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone.',
-        },
-        director: {
-            name: 'Quentin Tarantino',
-            bio: 'Quentin Jerome Tarantino (born March 27, 1963) is an American filmmaker, actor, film critic and author. His films are characterized by nonlinear storylines, dark humor, stylized violence, extended dialogue, pervasive use of profanity, ensemble casts, references to popular culture, alternate history, and neo-noir.',
-        },
-        imageUrl: 'https://www.imdb.com/title/tt0361748/mediaviewer/rm3163035648/'
-    },
-    {
-        title: 'Call Me by Your Name',
-        description: 'In 1980s Italy, romance blossoms between a seventeen-year-old student and the older man hired as his father\'s research assistant.',
-        genre: {
-            name: 'Romance',
-            description: 'Romance films or romance movies are romantic love stories recorded in visual media for broadcast in theaters and on television that focus on passion, emotion, and the affectionate romantic involvement of the main characters and the journey that their love takes them through dating, courtship or marriage.',
-        },
-        director: {
-            name:'Luca Guadagnino',
-            bio: 'Luca Guadagnino (born 10th August 1971) is an Italian film director, producer, and screenwriter. He has collaborated a number of times with actress Tilda Swinton, including on the films The Protagonists (1999), I Am Love (2009), A Bigger Splash (2015) and Suspiria (2018), a remake of the 1977 film of the same name.',
-        },
-        imageUrl: 'https://www.imdb.com/title/tt5726616/mediaviewer/rm1422667008/?ref_=tt_ov_i'
-    },
-    {
-        title: 'Into the Wild',
-        description: 'After graduating from Emory University, top student and athlete Christopher McCandless abandons his possessions, gives his entire $24,000 savings account to charity and hitchhikes to Alaska to live in the wilderness. Along the way, Christopher encounters a series of characters that shape his life.',
-        genre: {
-            name: 'Drama',
-            description: 'In film and television, drama is a category of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone.',
-        },
-        director: {
-            name: 'Sean Penn',
-            bio: 'Sean Justin Penn (born August 17, 1960) is an American actor, director, screenwriter, and producer. He has won two Academy Awards, for his roles in the mystery drama Mystic River (2003) and the biopic Milk (2008).',
-        },
-        imageUrl: 'https://www.imdb.com/title/tt0758758/mediaviewer/rm2115475456/?ref_=tt_ov_i' 
-    },
-    {
-        title: 'Lost in Translation',
-        description: 'A faded movie star and a neglected young woman form an unlikely bond after crossing paths in Tokyo.',
-        genre: {
-            name: 'Drama',
-            description: 'In film and television, drama is a category of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone.',
-        },
-        director: {
-            name: 'Sofia Coppola',
-            bio: 'Sofia Carmina Coppola (born May 14, 1971) is an American filmmaker and actress.' 
-        },
-        imageUrl: 'https://www.imdb.com/title/tt0335266/mediaviewer/rm4226351616/?ref_=tt_ov_i'
-    },
-    {
-        title: 'Babel',
-        description: 'Tragedy strikes a married couple on vacation in the Moroccan desert, touching off an interlocking story involving four different families.',
-        genre: 'Drama',
-        director: {
-            name: 'Alejandro G. Iñárritu',
-            bio: 'Alejandro González Iñárritu, (born 15 August 1963) is a Mexican film director, producer and screenwriter. He is primarily known for making modern psychological drama films about the human condition.',
-        },
-        imageUrl: 'https://www.imdb.com/title/tt0449467/mediaviewer/rm1692291584/?ref_=tt_ov_i'
-    }
-];
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //logging with Morgan
 app.use(morgan('common'));
 
 //get all static files from public dir
 app.use(express.static('public'));
-
-app.use(bodyParser.json());
 
 //Resquests
 // app.METHOD(PATH, HANDLER)
@@ -100,11 +33,18 @@ app.get('/', (req, res) => {
 
 //READ - Return a list of ALL movies to the user
 app.get('/movies', (req, res) => {
-    res.status(200).json(movies);
+    Movies.find()
+        .then((movies) => {
+            res.status(201).json(movies);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send("Error " + err);
+        });
 });
 
 //READ - Return data about a single movie by title to the user
-app.get('/movies/:title', (req, res) => {
+/*app.get('/movies/:title', (req, res) => {
     const { title } = req.params; //Object destructuring. Same as: const title = req.params.title;
     const movie = movies.find( movie => movie.title === title );
 
@@ -113,100 +53,167 @@ app.get('/movies/:title', (req, res) => {
     } else {
         res.status(400).send('movie does not exist');
     }
+});*/
+
+app.get('/movies/:Title', (req, res) => {
+    Movies.findOne({ Title: req.params.Title })
+        .then((movie) => {
+            res.status(200).json(movie);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error ' + err);
+        });
 });
 
-//READ - Return data about a genre by name/title
-app.get('/movies/genre/:genreName', (req, res) => {
-    const { genreName } = req.params;
-    const genre = movies.find( movie => movie.genre.name === genreName ).genre;
-
-    if (genre) {
-        res.status(200).json(genre);
-    } else {
-        res.status(400).send('genre does not exist');
-    }
+//READ - Return data about a genre by name
+app.get('/genre/:Name', (req, res) => {
+    Movies.findOne({ 'Genre.Name': req.params.Name }) //'Genre.Name' acess to the genre name to check matching with req
+        .then((movie) => {
+            res.status(200).json(movie.Genre); //movie.Genre acess to genre object (movie is the document name)
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error ' + err);
+        });
 });
 
 //READ - Return data about a director by name
-app.get('/movies/director/:directorName', (req, res) => {
-    const { directorName } = req.params;
-    const director = movies.find( movie => movie.director.name === directorName ).director;
+app.get('/director/:Name', (req, res) => {
+    Movies.findOne({ 'Director.Name': req.params.Name })
+        .then((movie) => {
+            res.status(200).json(movie.Director)
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error ' + err);
+        });
+});
 
-    if (director) {
-        res.status(200).json(director);
-    } else {
-        res.status(400).send('director does not exist');
-    }
+//READ - Return data about all users
+app.get('/users', (req, res) => {
+    Users.find()
+        .then((users) => {
+            res.status(201).json(users);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
+});
+
+///READ - Return data about user by username
+app.get('/users/:Username', (req, res) => {
+    Users.findOne({ Username: req.params.Username })
+        .then((user) => {
+            res.json(user);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
 });
 
 //CREATE - Allow new users to register
 app.post('/users', (req, res) => {
-    const newUser = req.body;
-
-    if(newUser.name) {
-        newUser.id = uuid.v4();
-        users.push(newUser);
-        res.status(201).json(newUser);
-    } else {
-        res.status(400).send('user need name');
-    }
+   Users.findOne({ Username: req.body.Username }) //check if username already exists
+    .then((user) => {
+        if (user) {
+            return res.status(400).send(req.body.Username + 'already exists');
+        } else {
+            Users
+                .create({
+                    Username: req.body.Username,
+                    Password: req.body.Password,
+                    Email: req.body.Email,
+                    Birthday: req.body.Birthday
+                })
+                .then((user) =>{res.status(201).json(user)
+                })
+                .catch((error) => {
+                    console.error(error);
+                    res.status(500).send('Error: ' + error);
+                });
+        }
+    })
+    .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+      });
 });
 
 //UPDATE - Allow users to update their user info (username)
-app.put('/users/:id', (req, res) => {
-    const { id } = req.params; //captures what was written in the URL
-    const updatedUser = req.body; //captures new user name the client wrote
-
-    let user = users.find( user => user.id == id); //take the right user from the array
-    
-    if (user) {
-        user.name = updatedUser.name //subsitute the old name for the new name
-        res.status(200).json(user);
-    } else {
-        res.status(400).send('no such user');
-    }
+app.put('/users/:Username', (req, res) => {
+    Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
+        {
+            Username: req.body.Username,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+        }
+    },
+    { new: true }, //make sure taht the updadted document is returned
+        (err, updatedUser) => {
+            if(err) {
+                console.error(err);
+                res.status(500).send('Error: ' + err);
+            } else {
+                res.json(updatedUser);
+            }
+        });
 });
-
-//CREATE - Allow users to add a movie to their list of favorites
-app.post('/users/:id/:movieTitle', (req, res) => {
-    const { id, movieTitle } = req.params; //captures what was written in the URL
-
-    let user = users.find( user => user.id == id); //take the right user from the array
     
-    if (user) {
-        user.favouriteMovies.push(movieTitle); //add movieTitle into the user/'s object --> favoutiteMovies
-        res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);
-    } else {
-        res.status(400).send('no such user');
-    }
+//UPDATE - Allow users to add a movie to their list of favorites
+app.post('/users/:Username/movies/:MovieID', (req, res) => {
+    Users.findOneAndUpdate({ Username: req.params.Username }, { $push:
+        {
+            FavoriteMovies: req.params.MovieID
+        },
+    },
+    { new: true },
+        (err, updateduser) => {
+            if(err) {
+                console.error(err);
+                res.status(500).send('Error: ' + err);
+            } else {
+                res.json(updateduser);
+            }
+        });
 });
 
 //DELETE - Allow users to remove a movie from their list of favorites
-app.delete('/users/:id/:movieTitle', (req, res) => {
-    const { id, movieTitle } = req.params; //captures what was written in the URL
-
-    let user = users.find( user => user.id == id); //take the right user from the array
-    
-    if (user) {
-        user.favouriteMovies = user.favouriteMovies.filter( title => title !== movieTitle); //remove movieTitle from user/'s object --> favoutiteMovies
-        res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`);
-    } else {
-        res.status(400).send('no such user');
-    }
+app.delete('/users/:Username/movies/:MovieID', (req, res) => {
+    Users.findOneAndUpdate({ Username: req.params.Username }, { $pull:
+        {
+            FavoriteMovies: req.params.MovieID
+        },
+    },
+    { new: true },
+        (err, deleteFavoriteMovie) => {
+            if(err) {
+                console.error(err);
+                res.status(500).send('Error: ' + err);
+            } else {
+                res.json(deleteFavoriteMovie);
+            }
+        });
 });
+    
 
 //DELETE - Allow existing users to deregister
-app.delete('/users/:id', (req, res) => {
-    const { id } = req.params; 
-
-    let user = users.find( user => user.id == id);
-    
-    if (user) {
-        users = users.filter( user => user.id != id); //remove user
-        res.status(200).send(`user ${id} has been deleted`);
-    } else {
-        res.status(400).send('no such user');
-    }
+app.delete('/users/:Username', (req, res) => {
+    Users.findOneAndRemove({ Username: req.params.Username })
+        .then((user) => {
+            if (!user) {
+                res.status(400).send(req.params.Username + ' was not found');
+            } else {
+                res.status(200).send(req.params.Username + ' was deleted.');
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        });
 });
 
 //Handling errors
